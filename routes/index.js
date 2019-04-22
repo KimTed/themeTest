1863ea5e2ec6ba08c22f6a4bd8a20b20debb2b4c
qp <ts.kim@gsshop.com>
2019-04-22 19:48:44 +0900

    feat: init

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('top50', { title: 'Express' });
+});
+
+//nocache 처리
+const nocache = (req, res, next) => {
+  res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+  res.header('Expires', '-1');
+  res.header('Pragma', 'no-cache');
+  next();
+}
+
+router.use('/api', nocache, require('./api'));
+
+module.exports = router;
